359f261110995c11a17d68c057176b783936797c
Danilo Martins <danfonmar@gmail.com>
2022-12-27 17:23:06 -0300

    Post, midleware e get

--- src/index.js
+++ src/index.js
@@ -1,25 +1,59 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  console.log('request.body', request.headers)
+
+  const usuario = users.find(user => user.username === username);
+  
+
+  if (!usuario) {
+    return response.status(404).json({Erro: "usuario não existe"})
+  }
+
+  request.usuario = usuario;
+
+  return next()
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  const userAlreadyExist = users.some(
+    (user) => user.username === username);
+    
+    if (userAlreadyExist) {
+      return response.status(400).json(
+        {Erro: "Usuário já cadastrado"})
+    };
+
+    users.push({
+      id: uuidv4(),
+      name,
+      username,
+      todo: []
+    });
+
+    return response.json(users);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+  const { usuario } = request
+
+  return response.json(usuario.todo)
+  
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
@@ -39,3 +73,4 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 module.exports = app;
+app.listen(3000);
